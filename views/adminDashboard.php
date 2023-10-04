7275bd3e49b83157788164dd0ae42370a0ec5fb2
Antonio <antonioboksic1603@gmail.com>
2023-10-04 21:00:22 +0200

    stampato tutti gli eventi in adminDashboard con metodo index del controller EventController.

--- views/adminDashboard.php
+++ views/adminDashboard.php
@@ -12,9 +12,42 @@ if(!isset($_SESSION['user_id']) || !$_SESSION['is_admin']) {
 }
 ?>
 
+<?php 
+include("../controllers/EventController.php");
+include('../database.php');
+$eventController = new EventController($pdo);
+$events = $eventController->index();
+?>
+
+<table border="1">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nome Evento</th>
+                <th>Data Evento</th>
+                <th>Partecipanti</th>
+                <th>Azioni</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($events as $event): ?>
+                <tr>
+                    <td><?= htmlspecialchars($event->id, ENT_QUOTES, 'UTF-8') ?></td>
+                    <td><?= htmlspecialchars($event->nome_evento, ENT_QUOTES, 'UTF-8') ?></td>
+                    <td><?= htmlspecialchars($event->data_evento, ENT_QUOTES, 'UTF-8') ?></td>
+                    <td><?= htmlspecialchars($event->attendees, ENT_QUOTES, 'UTF-8') ?></td>
+                    <td>
+                        <a href="editEvent.php?id=<?= $event->id ?>">Modifica</a>
+                        <a href="deleteEvent.php?id=<?= $event->id ?>">Elimina</a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</main>
 
 
     
-ciao sei un admin e puoi entrare
+
 </body>
 
